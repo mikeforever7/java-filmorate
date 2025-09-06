6d522b65c5874e976d90ee533269c6736c442391
Ilya <mikeforever7@yandex.ru>
2025-09-06 17:40:07 +0300

    Выполнил ФЗ12 поправил чекстайл

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Заполняем таблицу рейтингов
+MERGE INTO mpa_ratings (id, name) KEY (name) VALUES (1, 'G');
+MERGE INTO mpa_ratings (id, name) KEY (name) VALUES (2, 'PG');
+MERGE INTO mpa_ratings (id, name) KEY (name) VALUES (3, 'PG-13');
+MERGE INTO mpa_ratings (id, name) KEY (name) VALUES (4, 'R');
+MERGE INTO mpa_ratings (id, name) KEY (name) VALUES (5, 'NC-17');
+MERGE INTO genres (id, name) KEY (name) VALUES (1, 'Комедия');
+MERGE INTO genres (id, name) KEY (name) VALUES (2, 'Драма');
+MERGE INTO genres (id, name) KEY (name) VALUES (3, 'Мультфильм');
+MERGE INTO genres (id, name) KEY (name) VALUES (4, 'Триллер');
+MERGE INTO genres (id, name) KEY (name) VALUES (5, 'Документальный');
+MERGE INTO genres (id, name) KEY (name) VALUES (6, 'Боевик');
+INSERT INTO films (name, description, release_date, duration, mpa_rating_id) 
+VALUES ('Интерстеллар', 'Эпическая история о путешествии в космосе', '2014-11-07', 169, 3);

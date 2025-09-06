10f864693fe2b0a5e01fea28cd072b0531b5e047
Ilya <mikeforever7@yandex.ru>
2025-09-06 17:28:49 +0300

    Выполнил ФЗ12 поправил чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
